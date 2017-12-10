7a8d5e63b07efca6fba74fe5da16d24b1573eb1e
chebozh <ch_bozhinov@hotmail.co.uk>
2017-12-10 14:09:48 +0100

    Update ex35.py

--- ex35.py
+++ ex35.py
@@ -1,7 +1,9 @@
 """
 This exercise is Part 3 of 4 of the birthday data exercise series. The other exercises are: Part 1, Part 2, and Part 4.
 
-In the previous exercise we saved information about famous scientists’ names and birthdays to disk. In this exercise, load that JSON file from disk, extract the months of all the birthdays, and count how many scientists have a birthday in each month.
+In the previous exercise we saved information about famous scientists’ names and birthdays to disk.
+In this exercise, load that JSON file from disk, extract the months of all the birthdays, 
+and count how many scientists have a birthday in each month.
 
 Your program should output something like:
 
